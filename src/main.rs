a4d1871c34132ba4ea444e2f0293c5da3479343c
zenialexandre <zenialexandre123@gmail.com>
2025-02-17 16:58:31 -0300

    feat/build: adding new dependecies e first steps on managing windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app: App = App::default();
+    let _ = event_loop.run_app(&mut app);
+}
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, window_event: WindowEvent) {
+        match window_event {
+            WindowEvent::CloseRequested => {
+                println!("Close button pressed.");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => ()
+        }
+    }
 }
